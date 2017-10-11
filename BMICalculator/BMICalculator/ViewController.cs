6466c37e24956ec273e4c4424128c05e52479d0d
Blackbeardchris <christopherl10@learn.cssd.ab.ca>
2017-10-11 12:43:07 -0600

    fref

--- BMICalculator/BMICalculator/ViewController.cs
+++ BMICalculator/BMICalculator/ViewController.cs
@@ -35,5 +35,12 @@ namespace BMICalculator
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
         }
+
+        public override void TouchesBegan(Foundation.NSSet touches, UIEvent evt)
+        {
+            base.TouchesBegan(touches, evt);
+
+
+        }
     }
 }
